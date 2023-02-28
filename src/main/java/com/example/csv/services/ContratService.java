80cd4b602c60629359ff5f8a53916824ecd2298d
imenmzoughi <imenmzoughi1234@gmail.com>
2023-02-28 10:25:43 +0100

    first commi

--- src/main/java/com/example/csv/services/ContratService.java
+++ src/main/java/com/example/csv/services/ContratService.java
@@ -1,7 +1,11 @@
 package com.example.csv.services;
 
+import com.example.csv.domain.Contrat;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface ContratService {
     void save(MultipartFile file);
+    public List<Contrat> getAllContrat();
 }
