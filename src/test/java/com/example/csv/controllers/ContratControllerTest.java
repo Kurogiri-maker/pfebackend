d71ea053a437f6bc1ae782a48bf42de1d5a0bd6b
Kurogiri-maker <iheb.cherif99@gmail.com>
2023-03-14 09:12:48 +0100

    test controller

---
+++ src/test/java/com/example/csv/controllers/ContratControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.csv.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ContratControllerTest {
+    @Test
+    void getMetadata() {
+    }
+
+    @Test
+    void uploadFile() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getAllContrats() {
+    }
+
+    @Test
+    void getContrat() {
+    }
+
+    @Test
+    void deleteContrat() {
+    }
+
+    @Test
+    void updateTiers() {
+    }
+}
--- src/test/java/com/example/csv/controllers/DossierControllerTest.java
+++ src/test/java/com/example/csv/controllers/DossierControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.csv.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,4 +10,35 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Slf4j
 @ExtendWith(MockitoExtension.class)
 public class DossierControllerTest {
+    @Test
+    void getMetadata() {
+    }
+
+    @Test
+    void uploadFile() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getAllDossiers() {
+    }
+
+    @Test
+    void searchDossier() {
+    }
+
+    @Test
+    void getDossier() {
+    }
+
+    @Test
+    void deleteDossier() {
+    }
+
+    @Test
+    void updateDossier() {
+    }
 }
