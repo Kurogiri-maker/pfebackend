fa8966c2f09188b147b7499cdd8f5e9ec5a61aa1
TounsiAhmed <79357931+TounsiAhmed@users.noreply.github.com>
2023-03-24 12:44:20 +0100

    Update AuthenticationController.java to fix authenticate

--- src/main/java/com/example/csv/auth/AuthenticationController.java
+++ src/main/java/com/example/csv/auth/AuthenticationController.java
@@ -31,7 +31,7 @@ public class AuthenticationController {
         }
     }
 
-    @RequestMapping("/authenticate")
+    @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         try {
             AuthenticationResponse response = service.authenticate(request);
