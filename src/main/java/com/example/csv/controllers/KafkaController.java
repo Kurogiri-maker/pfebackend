168cb49adc7720bb1efeeb6cb4d145a2429f25df
Kurogiri-maker <iheb.cherif99@gmail.com>
2023-04-12 03:41:19 +0100

    Kafka consumer moved here

--- src/main/java/com/example/csv/controllers/KafkaController.java
+++ src/main/java/com/example/csv/controllers/KafkaController.java
@@ -1,6 +1,7 @@
 package com.example.csv.controllers;
 
 import com.example.csv.domain.KafkaResponse;
+import com.example.csv.helper.TopicListener;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -8,8 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
 import java.io.IOException;
 import java.util.Base64;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @Slf4j
@@ -18,6 +23,9 @@ public class KafkaController {
 
 
     private final WebClient webClient;
+
+    private final TopicListener topicListener;
+
 
     //Upload a document to collect its data
     @PostMapping("/kafka/collect")
@@ -49,15 +57,11 @@ public class KafkaController {
             // Return response body as the response to the client
             return  new ResponseEntity<>(responseBody,HttpStatus.OK);
 
-
-
-
-
     }
 
-    //Upload a document to collect its data
+    //Upload a document to get its type
     @PostMapping("/kafka/type")
-    public ResponseEntity<KafkaResponse> uploadFileForTypage(@RequestParam("file") MultipartFile fileResource){
+    public ResponseEntity<KafkaResponse> uploadFileForTypage(@RequestParam("file") MultipartFile fileResource) {
 
         byte[] fileContent;
         String message = "Please upload a pdf file!";
@@ -85,16 +89,20 @@ public class KafkaController {
 
             // Extract response body as String
             responseBody = responseSpec.bodyToMono(String.class).block();
-            message = "Uploaded the file successfully";
-            reponse.setMessage(message);
-            reponse.setResult(responseBody);
+            if (responseBody != null) {
+                // Kafka message processing logic
+                String processedResponse = topicListener.getTypageMessage();
 
-            // Return response body as the response to the client
-            return new ResponseEntity<>(reponse, HttpStatus.OK);
+                // Return response body as the response to the client
+                return new ResponseEntity<>(new KafkaResponse("Uploaded the file successfully",processedResponse), HttpStatus.OK);
+
+            }else{
+                // Handle exception as needed
+                return new ResponseEntity<>(new KafkaResponse( "Failed to process Kafka message",null), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
         }
 
         return new ResponseEntity<>(reponse,HttpStatus.BAD_REQUEST);
-
     }
 
     public  boolean hasPDFFormat(MultipartFile file) {
